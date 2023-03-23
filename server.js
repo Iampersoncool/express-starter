41b23fe8a3ce30d26d8c99695d1b6ea70c4eab37
Iampersoncool <86845476+Iampersoncool@users.noreply.github.com>
2023-03-22 17:09:56 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import apiRouter from './routes/ApiRouter.js'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+dotenv.config()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api', apiRouter)
+app.use((req, res) => res.status(404).send())
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
